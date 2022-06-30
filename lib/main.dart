194e2b947c49472e60b3caaae6c1c8eebf42ab6b
p_phanwang <p_phanwang@tencent.com>
2022-07-01 00:18:39 +0800

    update ui.

--- lib/main.dart
+++ lib/main.dart
@@ -113,160 +113,90 @@ class HomeState extends State<Home> {
         initLocalData();
         return true;
       },
-      child: SingleChildScrollView(
-        child: SizedBox(
-            height: MediaQuery.of(context).size.height,
-            child: Stack(
-              clipBehavior: Clip.none,
-              alignment: AlignmentDirectional.topStart,
+      child: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 160,
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            decoration: const BoxDecoration(color: Colors.blue),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 160,
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  decoration: const BoxDecoration(color: Colors.blue),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                GestureDetector(
+                  onTap: () async {
+                    isCustom = false;
+                    if (!await FlutterEasyPermission.has(
+                        perms: _permissions,
+                        permsGroup: _permissionGroup)) {
+                      FlutterEasyPermission.request(
+                          perms: _permissions,
+                          permsGroup: _permissionGroup);
+                    } else {
+                      startScan();
+                    }
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      GestureDetector(
-                        onTap: () async {
-                          isCustom = false;
-                          if (!await FlutterEasyPermission.has(
-                              perms: _permissions,
-                              permsGroup: _permissionGroup)) {
-                            FlutterEasyPermission.request(
-                                perms: _permissions,
-                                permsGroup: _permissionGroup);
-                          } else {
-                            startScan();
-                          }
-                        },
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SvgPicture.asset(
-                              "lib/assets/scan.svg",
-                              color: Colors.white,
-                              height: 48,
-                              width: 48,
-                            ),
-                            const SizedBox(height: 8),
-                            const Text(
-                              '扫一扫',
-                              style: TextStyle(color: Colors.white, fontSize: 16),
-                            )
-                          ],
-                        ),
+                      SvgPicture.asset(
+                        "lib/assets/scan.svg",
+                        color: Colors.white,
+                        height: 48,
+                        width: 48,
                       ),
-                      GestureDetector(
-                        onTap: () async {
-                          isCustom = true;
-                          if (!await FlutterEasyPermission.has(
-                              perms: _permissions,
-                              permsGroup: _permissionGroup)) {
-                            FlutterEasyPermission.request(
-                                perms: _permissions,
-                                permsGroup: _permissionGroup);
-                          } else {
-                            _openAliPay(
-                                'http://qrcode.sh.gov.cn/enterprise/scene');
-                          }
-                        },
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SvgPicture.asset(
-                              "lib/assets/health.svg",
-                              color: Colors.white,
-                              height: 48,
-                              width: 48,
-                            ),
-                            const SizedBox(height: 8),
-                            const Text(
-                              '健康码',
-                              style: TextStyle(color: Colors.white, fontSize: 16),
-                            )
-                          ],
-                        ),
+                      const SizedBox(height: 8),
+                      const Text(
+                        '扫一扫',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
                       )
-                      // ElevatedButton(
-                      //   child: const Text("扫场所码"),
-                      //   onPressed: () async {
-                      //     isCustom = false;
-                      //     if (!await FlutterEasyPermission.has(
-                      //         perms: _permissions,
-                      //         permsGroup: _permissionGroup)) {
-                      //       FlutterEasyPermission.request(
-                      //           perms: _permissions,
-                      //           permsGroup: _permissionGroup);
-                      //     } else {
-                      //       startScan();
-                      //     }
-                      //   },
-                      // ),
-                      // const SizedBox(
-                      //   width: 20,
-                      // ),
-                      // ElevatedButton(
-                      //   child: const Text("健康码(随申码)"),
-                      //   onPressed: () async {
-                      //     isCustom = true;
-                      //     if (!await FlutterEasyPermission.has(
-                      //         perms: _permissions,
-                      //         permsGroup: _permissionGroup)) {
-                      //       FlutterEasyPermission.request(
-                      //           perms: _permissions,
-                      //           permsGroup: _permissionGroup);
-                      //     } else {
-                      //       _openAliPay(
-                      //           'http://qrcode.sh.gov.cn/enterprise/scene');
-                      //     }
-                      //   },
-                      // ),
-                      // const SizedBox(
-                      //   width: 20,
-                      // ),
-                      // ElevatedButton(
-                      //     child: const Text("存数据"),
-                      //     onPressed: () async {
-                      //       QRCode qrCode = QRCode(
-                      //           id: DateTime.now().hashCode,
-                      //           description: "汤臣",
-                      //           qrCodeResult: "qrcode",
-                      //           createdTime: DateTime.now());
-                      //       QrCodeDatabase.instance.create(qrCode);
-                      //       QrCodeDatabase.instance
-                      //           .readAllQRCode()
-                      //           .then((value) => value.forEach((element) {
-                      //                 print(
-                      //                     '--julis  QrCodeDatabase.instance.readAllQRCode():${element.toJson()}');
-                      //               }));
-                      //     })
                     ],
                   ),
                 ),
-                Container(
-                  height: double.infinity,
-                  margin: const EdgeInsets.only(top: 160),
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(6),
-                        topRight: Radius.circular(6)),
-                  ),
+                GestureDetector(
+                  onTap: () async {
+                    isCustom = true;
+                    if (!await FlutterEasyPermission.has(
+                        perms: _permissions,
+                        permsGroup: _permissionGroup)) {
+                      FlutterEasyPermission.request(
+                          perms: _permissions,
+                          permsGroup: _permissionGroup);
+                    } else {
+                      _openAliPay(
+                          'http://qrcode.sh.gov.cn/enterprise/scene');
+                    }
+                  },
                   child: Column(
-                    children: items
-                        .asMap()
-                        .map<int, Widget>((i, item) =>
-                            MapEntry(i, PlaceItem(data: item, index: i)))
-                        .values
-                        .toList(),
-                    // items.map((c) => PlaceItem(data: items[index], index: index)).toList(),
-                  )),
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        "lib/assets/health.svg",
+                        color: Colors.white,
+                        height: 48,
+                        width: 48,
+                      ),
+                      const SizedBox(height: 8),
+                      const Text(
+                        '健康码',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      )
+                    ],
+                  ),
+                )
               ],
-            )),
-        ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, i)  {
+              return PlaceItem(data: items[i], index: i);
+            })
+          )
+        ],
+      ),
     );
   }
 
